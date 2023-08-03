c0a10ab88ecdaaaf87fe84a71cf32b8b4dd3d61b
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-08-03 12:09:06 -0400

    add ui for numbers

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [numbers, setNumbers] = useState([]);
+  useEffect(() => {
+    axios.get('/numbers').then(res => {
+      setNumbers(res.data);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Numbers :)</h1>
+      {numbers.map(number => <li key={number}>{number}</li>)}
     </div>
   );
 }
